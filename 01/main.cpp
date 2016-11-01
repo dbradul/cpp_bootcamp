0e85d60489405718de74cc7288ea4de57fc6879c
D B <onemoresunday@gmail.com>
2016-11-01 15:44:29 +0200

    added float examples (nan, inf)

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,7 +1,10 @@
 #include <cstdlib>
+#include <climits>
+#include <cmath>
 #include <iostream>
 #include <limits>
-#include <cmath>
+#include <iomanip>
+#include <bitset>
 
 using namespace std;
 
@@ -40,9 +43,11 @@ bool double_equals_sys(double a, double b, double epsilon = 0.001)
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "Hello world!" << std::endl;
+
+    //    return 0;
 
     cout << "==========================" << endl;
 
@@ -52,19 +57,21 @@ int main()
     std::cout << sizeof(int) << endl;
     std::cout << sizeof(unsigned int) << endl;
 
-
     cout << "==========================" << endl;
 
     cout << numeric_limits<int>::max() << endl;
-    cout << numeric_limits<int>::min()-1 << endl;
+    cout << numeric_limits<int>::min() << endl;
+    cout << "INT_MAX: " << INT_MAX << endl;
+    cout << "INT_MIN: " << INT_MIN << endl;
 
-    cout << numeric_limits<unsigned int>::max()+1 << endl;
-    cout << numeric_limits<unsigned int>::min() << endl;
 
-    cout << 0u-1 << endl;
-    cout << numeric_limits<unsigned int>::min()-1 << endl;
-    cout << -1u << endl;
-    cout << numeric_limits<unsigned int>::max()+1 << endl;
+    cout << numeric_limits<unsigned int>::max()/*+1*/ << endl;
+    cout << numeric_limits<unsigned int>::min()/*-1*/ << endl;
+
+    cout << "0u-1:  " << 0u-1 << endl;
+    cout << "-1u:   " << -1u << endl;
+    printf("-1u: %u\n", -1);
+
     foo(1);
     foo(1u);
 
@@ -73,14 +80,15 @@ int main()
     std::cout << sizeof(float) << endl;
     std::cout << sizeof(double) << endl;
 
-    std::cout << sizeof(char*) << endl;
-    std::cout << sizeof(short*) << endl;
-    std::cout << sizeof(int*) << endl;
-    std::cout << sizeof(long*) << endl;
-    std::cout << sizeof(float*) << endl;
-    std::cout << sizeof(double*) << endl;
+    cout << "==[FLOAT]========================" << endl;
 
-    cout << "==========================" << endl;
+    cout << std::numeric_limits<float>::epsilon() << endl;
+    cout << std::numeric_limits<float>::radix << endl;
+    cout << std::numeric_limits<float>::digits << endl;
+    cout << std::numeric_limits<float>::max_exponent << endl;
+    cout << std::numeric_limits<float>::min_exponent << endl;
+
+    cout << "==[DOUBLE]========================" << endl;
 
     cout << std::numeric_limits<double>::epsilon() << endl;
     cout << std::numeric_limits<double>::radix << endl;
@@ -88,16 +96,53 @@ int main()
     cout << std::numeric_limits<double>::max_exponent << endl;
     cout << std::numeric_limits<double>::min_exponent << endl;
 
-    //cout << ((double_equals(0.9, 1.4-0.2-0.3)) ? "TRUE" : "FALSE") << endl;
+    cout << ((0.9==1.4-0.2-0.3) ? "TRUE" : "FALSE") << endl;
+    float res = 1.4-0.2-0.3;
+    cout << setprecision(15) << res << endl;
 
     ////for(double d = 0; d != 0.3; d += 0.1);
 
-    ///
-    ///
+    cout << ((double_equals(0.9, 1.4-0.2-0.3)) ? "TRUE" : "FALSE") << endl;
 
-    cout << ((0.9==1.4-0.3-0.2) ? "TRUE" : "FALSE") << endl;
 
-    cout << "==========================" << endl;
+    {
+        cout << "==============================" << endl;
+        union S
+        {
+            unsigned int n;      // 4 bytes
+            float f;    // 4 bytes
+            char c;     // 1 byte
+        };              //  = total 4 bytes
+
+        S s;
+        s.n = 42;
+        cout << "s.f = " << s.f << endl;
+        s.n = 0xffffffff;
+        cout << "s.f = " << s.f << endl;
+        s.n = 4286578688;///2
+        cout << "s.f = " << s.f << endl;
+        s.n = 4286578688/2;///2
+        cout << "s.f = " << s.f << endl;
+        s.n = 2139095040;
+        cout << "s.f = " << s.f << endl;
+        s.f = -5;
+        cout << "s.n = " << s.n << endl;
+        s.f = 0.1;
+        cout << "s.n (0.1) = " << s.n << endl;
+        cout << "bin (0.1) = " << bitset<32>(s.n) << endl;
+
+        s.f = 0.15625;
+        cout << "s.n (0.1) = " << s.n << endl;
+
+        cout << "float 1./0 = " << 1.0/0 << endl;
+        cout << "float -1./0 = " << -1./0 << endl;
+        cout << "float 1/-1./0 = " << 1/(-1./0) << endl;
+        cout << "float -1/-1./0 = " << -1/(-1./0) << endl;
+        ////return 0;
+    }
+
+
+    cout << "=============bitwise=============" << endl;
     cout << 14/3 << endl;
     cout << 14./3 << endl;
     cout << 14%3 << endl;
@@ -142,7 +187,6 @@ int main()
         max_num_of_bits--;
     }
     cout << endl;
-
 
     cout << "==========================" << endl;
 
