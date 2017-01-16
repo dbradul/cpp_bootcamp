a6b9b838feb7ff83b13df7aae11226150c5cd2eb
D B <onemoresunday@gmail.com>
2017-01-17 00:00:14 +0200

    algorithms added

--- project_stl/main.cpp
+++ project_stl/main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <cassert>
-#include <vector>
-#include <list>
-#include <array>
-#include <algorithm>
-#include <functional>
-#include <array>
-#include <map>
-#include <unordered_map>
-#include <set>
+#include <bits/stdc++.h>
+#include <fstream>
+
+
+// Summary:
+//    - use vector as default container
+//    - use array when fixed size is needed
+//    - use map when association is needed
+//    - use unordered_map when speed is crititcal, but order is not
+//    - use set when need fast lookup/insert, order
+//    - use multi_* of the above when don't need uniqueness
+//    - use algorythm as the 1st resort
+
+// H/w:
+//  1. Sort word fr. in reversed order
+//  2. Concatenate all elements in vector<string>
+//  3. Create deque? (http://stackoverflow.com/questions/6292332/what-really-is-a-deque-in-stl)
 
 using namespace std;
+
+struct Box1
+{
+    int value;
+    char ch_value;
+};
 
 struct Box
 {
@@ -55,12 +69,12 @@ ostream& operator<< (ostream& os, const vector<T>& rhs)
     os << rhs.size() << ", " << rhs.capacity() << ": { ";
     for (const T& elem : rhs)
     {
-        os << elem << ", ";
+        os << (&elem!=&rhs[0] ? ", " : "");
+        os << elem;
     }
     os << " }";
     return os;
 }
-
 
 template<typename K, typename V,
                      typename Hash = std::hash<K>,
@@ -71,7 +85,8 @@ ostream& operator<< (ostream& os, const unordered_map<K, V, Hash, KeyEqual, Allo
     os << rhs.size() << ": { ";
     for (const pair<K, V>& elem : rhs)
     {
-        os << "(" << elem.first << "->" << elem.second << "), ";
+        os << (elem.first!=rhs.begin()->first ? ", " : "");
+        os << "(" << elem.first << "->" << elem.second << ")";
     }
     os << " }";
     return os;
@@ -83,7 +98,8 @@ ostream& operator<< (ostream& os, const map<K, V, Cmp>& rhs)
     os << rhs.size() << ": { ";
     for (const pair<K, V>& elem : rhs)
     {
-        os << "(" << elem.first << "->" << elem.second << "), ";
+        os << (elem.first!=rhs.begin()->first ? ", " : "");
+        os << "(" << elem.first << "->" << elem.second << ")";
     }
     os << " }";
     return os;
@@ -193,7 +209,6 @@ int main()
             cout << v << endl;
         }
 
-
         ////for(auto i = v.begin(); i != v.end(); ++i)
         for(auto i = v.begin(); i != v.end(); ++i)
         {
@@ -291,13 +306,24 @@ int main()
         }
     }
 
+    {
+        std::deque<int> d = {7, 5, 16, 8};
+
+        d.push_front(21);
+        d.push_back(42);
+
+        for(int n : d)
+        {
+            std::cout << n << '\n';
+        }
+    }
+
     // associative containers: map, mutlimup, set, multiset, unordered_*
     {
-//        map<string, Box, CmpLen> m = {  {"aaaa", {42}},
         map<string, Box, CmpLen> m = {
                                 {"aaaa", {42}},
-                                {"c", {43}},
-                                {"bbb", {41}},
+                                {"c",    {43}},
+                                {"bbb",  {41}},
                              };
 
         for (auto it=m.begin(); it!=m.end(); ++it)
@@ -321,7 +347,6 @@ int main()
 
             cout << m << endl;
         }
-
     }
 
     {
@@ -344,14 +369,6 @@ int main()
         cout << m << endl;
     }
 
-    {
-        set<string, CmpLen> s = { "aaaa", "c", "bbb" };
-
-        for (auto it=s.begin(); it!=s.end(); ++it)
-        {
-            cout << *it << endl;
-        }
-    }
 
     {
         unordered_map<string, int, HashT, EqualT> m = {
@@ -375,28 +392,211 @@ int main()
         cout << m << endl;
     }
 
+
+    {
+        // count words
+        unordered_map<string, int> words_count;
+
+        ifstream file;
+        file.open("/home/db/test2.txt");
+        string word;
+
+        while (file >> word)
+        {
+            int value = words_count[word];
+            words_count[word]++;
+        }
+
+        for(auto entry : words_count)
+        {
+            cout << entry.first << ": " << entry.second << endl;
+        }
+
+        unordered_set<string/*, CmpLen*/> s = { "aaaa", "c", "bbb" };
+        unordered_map<string, int> m = { {"aaaa", 42},
+                               {"c", 3},
+                               {"bbb", 33} };
+
+        for (auto it=s.begin(); it!=s.end(); ++it)
+        {
+            cout << *it << endl;
+        }
+
+        for (auto it=m.begin(); it!=m.end(); ++it)
+        {
+            cout << it->first << endl;
+        }
+    }
+
+
+    {
+        set<string/*, CmpLen*/> s1 = { "aaaa", "c", "bbb" };
+
+        set<string/*, CmpLen*/> s2 = { "a", "c", "c", "c", "c", "bb" };
+
+        for (auto it=s2.begin(); it!=s2.end(); ++it)
+        {
+            cout << *it << endl;
+        }
+
+        vector<string> v;
+        std::set<string/*, CmpLen*/> result;
+        std::set_difference(s1.begin(), s1.end(),
+                            s2.begin(), s2.end(),
+                            std::back_inserter(v));
+//                            v.begin());
+//                            std::inserter(result, result.end()));
+
+        cout << "..........................." << endl;
+        for (auto elem : v)
+        {
+            cout << elem << endl;
+        }
+        cout << "..........................." << endl;
+
+    }
+
+
+    {
+        std::vector<int> v1{1,2,3,4,5,6,7,8     };
+        std::set<int>    v2{        5,  7,  9,10};
+        std::sort(v1.begin(), v1.end());
+        //std::sort(v2.begin(), v2.end());
+
+        std::vector<int> xor_v;
+
+        std::set_symmetric_difference(
+            v1.begin(), v1.end(),
+            v2.begin(), v2.end(),
+            std::back_inserter(xor_v));
+
+        cout << xor_v << endl;
+     }
+
+
+
     // algorythms
     {
+        // non-modifying
         std::vector<int> v{1,1,1,1,1,1};
-        std::vector<int> v2(1);
+        std::vector<int> v2(v.size());
 
         cout << v << endl;
+        std::for_each(v.begin(),
+                      v.end(),
+                      [](int &n){ n++; });
 
-        std::for_each(v.begin(), v.end(), [](int &n){ n++; });
         Sum s = std::for_each(v.begin(), v.end(), Sum());
 
         cout << v << endl;
         cout << s.sum << endl;
 
-        std::transform(v.begin(), v.end(), v2.begin(), [](int &n){ return n*n; });
-        cout << v << endl;
-        cout << v2 << endl;
-
-        int sum = accumulate(v.begin(), v.end(), 0);
-        int product = accumulate(v.begin(), v.end(), 1, [](int &a, int &b){ return a*b; });
+        int sum     = accumulate(v.begin(), v.end(), 0);
+        int subtr   = accumulate(v.begin(), v.end(), 0, minus<int>());
+        int product = accumulate(v.begin(), v.end(), 1, multiplies<int>());
+        int product2= accumulate(v.begin(), v.end(), 1, [](const int& a, const int& b){ return a*b; });
+        int min     = accumulate(v.begin(), v.end(), v[0], [](const int& a, const int& b){ return ::min(a,b);});
+        int max     = accumulate(v.begin(), v.end(), v[0], [](const int& a, const int& b){ return ::max(a,b);});
 
         cout << sum << endl;
+        cout << subtr << endl;
         cout << product << endl;
+        cout << product2 << endl;
+        cout << min << endl;
+        cout << max << endl;
+
+        vector<int> vv = v;
+        auto it = min_element(vv.begin(), vv.end());
+
+        if (it != vv.end())
+        {
+            cout << *it << endl;
+        }
+        else
+        {
+            cout << "not found!" << endl;
+        }
+
+        {
+            std::list<int> l = { 0, 2, 5, 6, 3 }; // C++11 initializer lists
+            std::vector<int> v = { 0, 2, 5, 6, 3 };  // C++11 initializer lists
+            std::array<int, 5> a = { 0, 2, 5, 6, 3 };
+
+            cout << (std::find(l.begin(), l.end(), 1) != l.end()) << endl;
+            cout << (std::find(v.begin(), v.end(), 1) != v.end()) << endl;
+            cout << (std::find(a.begin(), a.end(), 1) != a.end()) << endl;
+
+            cout << "Count if" << endl;
+            cout << std::count_if(v.begin(), v.end(), [](const int& value){return value%2==0;}) << endl;
+        }
+
+        // find
+        // find_if
+        // equal
+        // equal_if
+        // all_of
+
+        vector<Box> v0 = {{1},{2},{3}};
+        cout << any_of(v0.begin(), v0.end(), [](const Box& b){return b.value%2==0;}) << endl;
+
+        // modifying
+        std::fill (v.begin(),v.end()-1, 5);
+        cout << v << endl;
+
+        vector<int> v3 = v;
+        std::transform(v.begin(), v.end(),
+                       v3.begin(),
+                       v3.begin(),
+                       [](int &n, int &n1){ return n+n1; });
+
+        cout << v << endl;
+        cout << v2 << endl;
+        cout << v3 << endl;
+
+        // replace
+        // replace_if
+        // copy
+        // copy_if
+        vector<Box> v4 = {{1},{2},{3}};
+        vector<Box> v5 = {{42}};
+        copy_if(v4.begin(), v4.end(),
+                back_inserter(v5),
+                //v5.begin(),
+                [](const Box& b){return b.value%2==0;});
+
+        cout << v5 << endl;
+
+        // sorting+relational
+        {
+            std::vector<int> v1{9,2,2,2,3,4,5,6,7,8,1};
+            std::vector<Box1> v2{
+                {2,'d'},
+                {10,'a'},
+                {13,'e'},
+                {2,'c'},
+                {10,'b'},
+                {13,'f'},
+                {2,'1'},
+                {1,'2'},
+                {33,'3'},
+                {2,'4'},
+                {1,'5'},
+                {33,'6'},
+            };
+            std::sort(v2.begin(), v2.end(), [](Box1 &a, Box1 &b){ return a.value<b.value; });
+            for_each (v2.begin(), v2.end(),
+                      [](Box1 &a){cout << a.value << ": " << a.ch_value << endl;});
+
+            Box1 b1 = {33,'6'};
+            bool b = binary_search(
+                          v2.begin(),
+                          v2.end(),
+                          b1,
+                          [](const Box1 &a, const Box1 &b){ return a.value==b.value; });
+
+//            lower_bound
+//            nth_element
+        }
     }
 
     return 0;
